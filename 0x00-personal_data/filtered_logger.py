6e5217dcc7846352f094a928f17570145b990359
prototypedave <popupgfj@gmail.com>
2022-09-04 00:14:31 +0000

    regexing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,74 +1,15 @@
 #!/usr/bin/env python3
 """
-Regexing
+REGEX-ING
 """
 
-from typing import List
 import re
-import logging
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class """
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """  Log formatter """
-        return filter_datum(self.fields, self.REDACTION,
-                            super().format(record), self.SEPARATOR)
+from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    """ returns the log message obfuscated """
+    """ returns obfuscated values of fields """
     for item in fields:
-        message = re.sub(fr'{item}=.+?{separator}',
-                         f'{item}={redaction}{separator}', message)
+        message = re.sub(r'{}=.*?{}'.format(item, separator),
+                         item + "=" + redaction + separator, message)
     return message
-
-
-def get_logger() -> logging.Logger:
-    """ returns a logging.Logger object """
-    logger = logging.getLogger("user_data")
-    logger.setLevel(logging.INFO)
-    logger.propagate = False
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    logger.addHandler(handler)
-    return logger
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """ returns a connector to the database """
-    return mysql.connector.connect(
-                    host=os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost'),
-                    database=os.environ.get('PERSONAL_DATA_DB_NAME', 'root'),
-                    user=os.environ.get('PERSONAL_DATA_DB_USERNAME'),
-                    password=os.environ.get('PERSONAL_DATA_DB_PASSWORD', ''))
-
-
-def main():
-    """ obtain a database connection using get_db and retrieve all rows in the
-        users table and display each row under a filtered format """
-    db = get_db()
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM users;")
-    result = cursor.fetchall()
-    for row in result:
-        message = f"name={row[0]}; " + \
-                  f"email={row[1]}; " + \
-                  f"phone={row[2]}; " + \
-                  f"ssn={row[3]}; " + \
-                  f"password={row[4]};"
-        print(message)
-        log_record = logging.LogRecord("my_logger", logging.INFO,
-                                       None, None, message, None, None)
-        formatter = RedactingFormatter(PII_FIELDS)
-        formatter.format(log_record)
-    cursor.close()
-    db.close()
